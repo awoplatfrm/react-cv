63ab6ff565cbbbd7016bcdca856ec45945971f1b
awoplatfrm <awoplatfrm@yahoo.com>
2026-02-05 11:34:57 +0100

    BrowserRouting fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import CVLayout from './components/CvLayout';
 import './App.css';
 
 function App() {
   return (
-    <Router basename='/react-cv'>
+    <HashRouter>
       <div className="App">
         <CVLayout />
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
